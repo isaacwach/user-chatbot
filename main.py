a0b973b6beefc8572dfa0fe75fdc9a4757c6a4f0
isaacwach <isaacwachira57@gmail.com>
2024-01-03 11:20:18 +0300

    created a knowledge base file to store the data in jsonformat and created a funtion to load the knowledge base and return data

---
+++ main.py
@@ -0,0 +1,7 @@
+import json
+from difflib import get_close_matches
+
+def load_knowledge_base(file_path: str)->dict:
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+    return data
