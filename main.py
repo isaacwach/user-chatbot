8e40ae518abc25c53247e9cf7e6d24905d36abf6
isaacwach <isaacwachira57@gmail.com>
2024-01-03 11:24:13 +0300

    created a function to save data from user input into the knowledge base file

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def load_knowledge_base(file_path: str)->dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
     return data
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
