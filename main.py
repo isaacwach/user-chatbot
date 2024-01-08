c42acf7028996bdbefbf61faf8ce010bc1875504
isaacwach <isaacwachira57@gmail.com>
2024-01-08 08:45:33 +0300

    created conditional statements to print out bot answer based on user input

--- main.py
+++ main.py
@@ -28,3 +28,17 @@ def chat_bot():
         if user_input.lower() == 'quit':
             break
         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f'Bot: {answer}')
+        else:
+            print('Bot: I dont know the answer. Can you train me?')
+            new_answer: str = input('Type the answer or "skip" to skip')
+
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print('Bot: Thank you: I learned a new response:')
+
+if __name__ == '__main__':
+    chat_bot()
